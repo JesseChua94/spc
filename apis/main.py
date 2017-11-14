3b8ea619df8103a939dd9a400a77991315a15154
Jesse Chua <jesse.chua@mail.utoronto.ca>
2017-11-13 20:03:39 -0500

    Added base layout for main page

--- apis/main.py
+++ apis/main.py
@@ -5,4 +5,5 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def test():
-    return render_template('main.html')
+    return render_template('main.html', videos=[["active",1,2,3,4],["",5,6,7,8]])
+
